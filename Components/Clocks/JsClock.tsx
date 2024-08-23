15fd4107a3592f207baf144ea188566ad1f15bf2
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2024-08-22 22:10:32 -0500

    feat: Update page layout and metadata
    - Set the width of the body element to 100% and the minimum height to 100vh in the globals.css file.
    - Update the title and description in the metadata object in the layout.tsx file.

---
+++ Components/Clocks/JsClock.tsx
@@ -0,0 +1,74 @@
+export default function JsClock({ time }: Readonly<{ time: Date }>) {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <code className="text-2xl">
+        <KeyWord value="const" /> <Const value="clock" />{" "}
+        <Operator value="=" /> <Brackets value="{" />
+        <NumberProperty name="hour" value={time.getHours()} />
+        <NumberProperty name="minute" value={time.getMinutes()} />
+        <NumberProperty name="second" value={time.getSeconds()} />
+        <StringProperty name="period" value={time.getHours() >= 12 ? "PM" : "AM"} />
+        <NumberProperty name="day" value={time.getDate()} />
+        <StringProperty name="month" value={time.toLocaleString("en-GB", { month: "long" })} />
+        <NumberProperty name="year" value={time.getFullYear()} />
+        <Brackets value="}" />
+
+      </code>
+    </div>
+  )
+}
+
+const Tab = ({ children }: Readonly<{ children: React.ReactNode }>) => (
+  <div className="pl-4">{children}</div>
+)
+
+const KeyWord = ({ value }: Readonly<{ value: string }>) => (
+  <span className="text-[#4A9CD6]">{value}</span>
+)
+
+const Const = ({ value }: Readonly<{ value: string }>) => (
+  <span className="text-[#44C1FF]">{value}</span>
+)
+const Variable = ({ text }: Readonly<{ text: string }>) => (
+  <span className="text-[#9CDCFE]">{text}</span>
+)
+
+const Operator = ({ value }: Readonly<{ value: string }>) => (
+  <span className="text-[#D4D4D4]">{value}</span>
+)
+
+const Brackets = ({ value }: Readonly<{ value: string }>) => (
+  <span className="text-[#F8D700]">{value}</span>
+)
+
+const NumberValue = ({ value }: Readonly<{ value: number }>) => (
+  <span className="text-[#A7CEA8]">{value}</span>
+)
+
+const StringValue = ({ value }: Readonly<{ value: string }>) => (
+  <span className="text-[#CE9178]">&quot;{value}&quot;</span>
+)
+
+const NumberProperty = ({
+  name,
+  value,
+}: Readonly<{ name: string; value: number }>) => (
+  <Tab>
+    <Variable text={name} />
+    <Operator value=": " />
+    <NumberValue value={value} />
+    <Operator value="," />
+  </Tab>
+)
+
+const StringProperty = ({
+  name,
+  value,
+}: Readonly<{ name: string; value: string }>) => (
+  <Tab>
+    <Variable text={name} />
+    <Operator value=": " />
+    <StringValue value={value} />
+    <Operator value="," />
+  </Tab>
+)
