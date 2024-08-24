b220a262fe2f12c975b0f17e9504780d50a14006
Andres Felipe Martinez Salazar <andresfelipe.2031@gmail.com>
2024-08-23 19:37:11 -0500

    feat: Add dynamic clock selection and improve clock components

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,17 @@ import JsClock from "@/components/JsClock"
 import PyClock from "@/components/PyClock"
 import { useEffect, useState } from "react"
 
-type Clock = ({ time }: Readonly<{ time: Date }>) => JSX.Element
-
 const clocks = {
-  js: JsClock,
-  py: PyClock,
+  js: { name: "JavaScript", Component: JsClock },
+  py: { name: "Python", Component: PyClock },
 } as const
+type Clock = (typeof clocks)[keyof typeof clocks]
 
 type Clocks = keyof typeof clocks
 
 export default function Home() {
   const [time, setTime] = useState(new Date())
-  const [clock, setClock] = useState<keyof typeof clocks>("py")
+  const [Clock, setClock] = useState<Clock>(clocks.js)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -25,23 +24,27 @@ export default function Home() {
   }, [])
 
   const onChange: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-    setClock(e.currentTarget.value as Clocks)
+    setClock(clocks[e.currentTarget.value as Clocks])
   }
-
-  const Clock = clocks[clock]
 
   return (
     <main className="flex max-h-screen min-h-screen flex-col items-center p-24">
-      <div className="flex flex-wrap gap-4">
-        <button value="js" onClick={onChange}>
-          JavaScript
-        </button>
-        <button value="py" onClick={onChange}>
-          Python
-        </button>
+      <div className="flex flex-wrap gap-12">
+        {Object.keys(clocks).map((key) => (
+          <div key={key} className="group relative">
+            <div className="absolute -inset-1 rounded-lg bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500 opacity-75 blur transition duration-500 group-hover:opacity-100"></div>
+            <button
+              value={key}
+              onClick={onChange}
+              className="relative rounded-lg bg-black px-7 py-4 text-white"
+            >
+              {clocks[key as Clocks].name}
+            </button>
+          </div>
+        ))}
       </div>
       <div className="flex flex-1 w-full justify-center items-center">
-        <Clock time={time} />
+        <Clock.Component time={time} />
       </div>
       <div></div>
     </main>
